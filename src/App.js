6862a09e761bd1ce29904337eec72ccbb03f7c23
seokgeun <trr456@naver.com>
2023-02-20 22:27:09 +0900

    firebase SDK 추가 react-router-dom 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/login/Login";
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
