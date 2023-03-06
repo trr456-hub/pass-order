ea1432a4fdf1ed7c180d6a9533e73e8bcf940bd8
gorhf9397 <trr456@naver.com>
2023-03-06 16:33:17 +0900

    사이즈별 가격 프린트 onclick이벤트 실행시 실행값에 맞는 데이터 출력 수량 탭 추가

--- src/components/Order/MenuDetail.jsx
+++ src/components/Order/MenuDetail.jsx
@@ -1,20 +1,59 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faS, faM, faL } from "@fortawesome/free-solid-svg-icons";
+import {
+  faS,
+  faM,
+  faL,
+  faPlus,
+  faMinus,
+} from "@fortawesome/free-solid-svg-icons";
 
 const MenuDetail = () => {
+  /** sizeSmall에 state를 담아주는 hook */
   const [small, setSmall] = useState(0);
+  /** sizeLarge에 state를 담아주는 hook */
+  const [large, setLarge] = useState(0);
   const location = useLocation();
   const itemCode = location.state.itemcode;
   // console.log(itemCode);
-  useEffect(() => {
+
+  const sizeClick = (e) => {
+    // if (e.target.tagName !== "BUTTON") {
+    //   return;
+    // }
+    const btnType = e.target.getAttribute("data-type");
     let price = itemCode.price;
-    if (price >= 2000 && price < 3000) {
-      const s = price - 300;
-      setSmall(s);
+    if (btnType === "small") {
+      price = small;
+    } else if (btnType === "midium") {
+      price = itemCode.price;
+    } else if (btnType === "large") {
+      price = large;
     }
-  }, []);
+    console.log(price);
+  };
+
+  useEffect(() => {
+    /** size small 의 가격을 정의 해주는 함수 */
+    const smallPrice = () => {
+      const price = itemCode.price;
+      if (price < 2000) {
+        setSmall(price - 300);
+      } else if (price >= 2000 && price < 3000) {
+        setSmall(price - 400);
+      } else if (price > 3000) {
+        setSmall(price - 500);
+      }
+    };
+    /** size large 의 가격을 정의 해주는 함수 */
+    const largePrice = () => {
+      const price = itemCode.price;
+      setLarge(price + 1000);
+    };
+    largePrice();
+    smallPrice();
+  }, [itemCode.price]);
   return (
     <div className="menuContainer">
       <header className="menuHeader">{itemCode.name}</header>
@@ -27,17 +66,29 @@ const MenuDetail = () => {
         <div className="sizeContainer">
           <span>SIZE</span>
           <div className="sizeBtnContainer">
-            <button>
-              <FontAwesomeIcon icon={faS} />
-              <p>{small}</p>
+            <button onClick={sizeClick} data-type="small">
+              <FontAwesomeIcon icon={faS} data-type="small" />
+              <p data-type="small">{small}원</p>
             </button>
-            <button>
-              <FontAwesomeIcon icon={faM} />
-              <p>{itemCode.price}</p>
+            <button onClick={sizeClick} data-type="midium">
+              <FontAwesomeIcon icon={faM} data-type="midium" />
+              <p data-type="midium">{itemCode.price}원</p>
             </button>
+            <button onClick={sizeClick} data-type="large">
+              <FontAwesomeIcon icon={faL} data-type="large" />
+              <p data-type="large">{large}원</p>
+            </button>
+          </div>
+        </div>
+        <div className="menuNumber">
+          <span>수량</span>
+          <div>
             <button>
-              <FontAwesomeIcon icon={faL} />
-              <p>(+1000)</p>
+              <FontAwesomeIcon icon={faMinus} />
+            </button>
+            <span>4</span>
+            <button>
+              <FontAwesomeIcon icon={faPlus} />
             </button>
           </div>
         </div>
