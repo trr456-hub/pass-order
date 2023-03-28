b35d906160f5f131b06d4c9418345b8aa15c3ffc
gorhf9397 <trr456@naver.com>
2023-03-28 18:03:30 +0900

    주문내역에 모든컬렉션 조건에맞는 데이터가 있는지 검사한후 배열에 요소가 있는지확인후 뿌려줌

--- src/components/Home/OrderItem.jsx
+++ src/components/Home/OrderItem.jsx
@@ -1,42 +1,125 @@
-import {
-  collection,
-  getDocs,
-  getFirestore,
-  query,
-  where,
-} from "firebase/firestore";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { dbService } from "fbase";
+import { getDatabase, onValue, ref } from "firebase/database";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
+import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 
 const OrderItem = ({ userObj }) => {
   const [stores, setStores] = useState([]);
-  const [numbers, setNumbers] = useState([]);
+  const [store, setStore] = useState({});
 
-  const userId = userObj.uid;
+  const navigation = useNavigate();
+  const userId = userObj.displayName;
+
+  const querySnapshot = useCallback(async () => {
+    const storesArr = stores.map((item) => item.number.toString());
+    const storeIndex = storesArr.map((item) =>
+      query(collection(dbService, item), where("user", "==", userId))
+    );
+    const storeCollection = await Promise.all(
+      storeIndex.map(async (item) => await getDocs(item))
+    );
+    const storeData = storeCollection.map((doc) =>
+      doc.docs.map((d) => d.data())
+    );
+    const storeSelect = storeData.filter((arr) => arr.length > 0);
+    // 배열에 요소가 있는지 확인
+    if (storeSelect.length > 0) {
+      const storeItem = storeSelect[0];
+      setStore(storeItem[0]);
+    }
+  }, [stores, userId]);
 
   useEffect(() => {
-    stores.forEach((item) => {
-      setNumbers(item.number);
+    querySnapshot();
+  }, [querySnapshot]);
+
+  useEffect(() => {
+    const db = getDatabase();
+    const storesRef = ref(db, "passOrder/stores");
+    //db호출 여 매장정보 프린팅
+    onValue(storesRef, (snapshot) => {
+      const data = snapshot.val();
+      const storesList = Object.values(data);
+      setStores(storesList);
     });
-  }, [stores]);
+  }, []);
 
-  useEffect(() => {
-    const db = getFirestore();
-    const storesCollection = collection(db, "passOrder/stores");
-    const storesQuery = query(storesCollection, where("uid", "==", userId));
-    getDocs(storesQuery)
-      .then((querySnapshot) => {
-        const storesList = [];
-        querySnapshot.forEach((doc) => {
-          storesList.push(doc.data());
-        });
-        setStores(storesList);
-      })
-      .catch((error) => {
-        console.log("Error getting documents: ", error);
-      });
-    console.log(numbers);
-  }, [userId]);
-  return <div>오더아이템</div>;
+  return (
+    <div className="orderListContainer">
+      <header className="menuHeader">
+        <div
+          onClick={() => navigation("/")}
+          className="backArrow"
+          style={{ fontSize: 20 }}
+        >
+          <FontAwesomeIcon icon={faHome} />
+        </div>
+        <span>주문내역</span>
+      </header>
+      <div className="orderListElement">
+        <div className="orderListStore">
+          <span>매장정보</span>
+          {store.store && (
+            <>
+              <h1>
+                매장명<span>{store.store.name}</span>
+              </h1>
+              <h1>
+                매장주소<span>{store.store.address}</span>
+              </h1>
+            </>
+          )}
+          <h1>
+            수령시간<span>준비완료 후 수령 가능</span>
+          </h1>
+        </div>
+        <div className="orderItem">
+          <span>주문내역</span>
+          <div className="orderitemElement">
+            <div>
+              <span>품명</span>
+              <span>사이즈</span>
+              <span>타입</span>
+              <span>수량</span>
+              <span>합계</span>
+            </div>
+            {store.order &&
+              store.order.map((item, i) => (
+                <div key={i}>
+                  <span>{item.name}</span>
+                  <span>{item.size}</span>
+                  <span>{item.type}</span>
+                  <span>{item.number}</span>
+                  <span>{item.price} 원</span>
+                </div>
+              ))}
+          </div>
+        </div>
+        <div className="orderListInform">
+          <span>주문정보</span>
+          <h2>
+            닉네임<span>{store.user}</span>
+          </h2>
+          <h2>
+            요청사항<span>{store.request}</span>
+          </h2>
+          <h2>
+            적립스탬프<span>{store.stamp} 개</span>
+          </h2>
+          <h2>
+            결제금액<span>{store.total} 원</span>
+          </h2>
+        </div>
+        <div className="orderListComent">
+          <span>* 새로운 주문 시 마지막 주문내역은 사라집니다. *</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OrderItem;
