a4faa5625dbc76edee6b7428bccac0570b42fd12
gorhf9397 <trr456@naver.com>
2023-04-06 22:18:44 +0900

    111

--- src/index.js
+++ src/index.js
@@ -14,11 +14,6 @@ import "./scss/Order/Basket.scss";
 import "./scss/Order/Payment.scss";
 import "./scss/Order/OrderList.scss";
 import "./scss/Seller/Seller.scss";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
-  </BrowserRouter>
-);
+root.render(<App />);
