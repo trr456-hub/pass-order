931141708f54527a70e4a7ea836277796492c5ef
gorhf9397 <trr456@naver.com>
2023-04-06 22:21:29 +0900

    as

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home/Home";
 // 로그인 컴포넌트
 import LoginHome from "./Login/LoginHome";
